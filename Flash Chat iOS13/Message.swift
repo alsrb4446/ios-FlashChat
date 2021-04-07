05f5610281fcf2e2d18ab42fd72e2f70a65f9483
alsrb4446 <alsrbb4446@naver.com>
2021-04-07 17:29:31 +0900

    Chatting app with Firebase

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by 신민규 on 2021/04/05.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String //1@2.com
+    let body: String
+}
